e5fe8b66e6801b3f985db389ed88b5d6cd3375d9
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-08-07 21:57:38 -0700

    Create __init__.py

---
+++ SDSongBot/__init__.py
@@ -0,0 +1,13 @@
+#SDBOTs <https://t.me/SDBOTs_Inifinity>
+
+import logging
+from pyrogram import Client
+from config import API_HASH, API_ID, BOT_TOKEN
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+LOGGER = logging.getLogger(__name__)
+
+SDbot = Client("SDSongBot", bot_token=BOT_TOKEN, api_hash=API_HASH, api_id=API_ID)
