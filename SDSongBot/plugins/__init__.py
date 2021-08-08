4a6474cae1857a017129f32e87cf647902323efa
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-08-07 21:56:59 -0700

    Create __init__.py

---
+++ SDSongBot/plugins/__init__.py
@@ -0,0 +1,9 @@
+#SDBOTs <https://t.me/SDBOTs_Inifinity>
+
+from os.path import dirname, basename, isfile, join
+import glob
+
+modules = glob.glob(join(dirname(__file__), "*.py"))
+__all__ = [
+    basename(f)[:-3] for f in modules if isfile(f) and not f.endswith("__init__.py")
+]
