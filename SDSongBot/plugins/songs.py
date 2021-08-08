41c1381c949708c14d1ab52586af885a4a5044c4
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-08-07 22:06:06 -0700

    Update songs.py

--- SDSongBot/plugins/songs.py
+++ SDSongBot/plugins/songs.py
@@ -6,7 +6,7 @@ import requests
 import aiohttp
 import youtube_dl
 
-from SDSongBot import Jebot as app
+from SDSongBot import SDbot as app
 from pyrogram import filters, Client
 from youtube_search import YoutubeSearch
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputTextMessageContent
